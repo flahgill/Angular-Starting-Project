eaf4a4510d3264cf70c4074f521df75d65a4af52
Gillan Flaherty <flahertygillian@gmail.com>
2024-07-25 10:26:16 -0500

    updated userComp input to be User object instead of the attributes, and defined the User interface object type

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,16 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string; 
+//   name: string; 
+//   avatar: string
+// };
+
+interface User {
+  id: string; 
+  name: string; 
+  avatar: string
+};
 
 @Component({
   selector: 'app-user',
@@ -8,16 +19,15 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-      this.select.emit(this.id);
+      this.select.emit(this.user.id);
   }
 }
